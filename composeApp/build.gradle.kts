ef94cb6a2905a99872bb4bdcb458e10638fee23a
jonesmbindyo <jonesmbindyo@gmail.com>
2026-03-05 12:06:29 +0300

    Update project version and documentation from dev to staging

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0.1-dev"
+        versionName = "1.0.1-staging"
     }
     packaging {
         resources {
